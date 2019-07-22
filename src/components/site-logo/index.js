5751a74757ab808125e86544fdcd6576aaf0292e
Simon Hudson <simon.hudson@experian.com>
2019-07-22 09:41:45 +0100

    Use CSS for grey footer image

--- src/components/site-logo/index.js
+++ src/components/site-logo/index.js
@@ -2,13 +2,12 @@
 
 import React from 'react';
 import logo from './imgs/logo.svg';
-import logoGrey from './imgs/logo--grey.svg';
 import css from './css/styles.scss';
 
 const SiteLogo = props => {
 	
 	return (
-		<img alt="" className={`site-logo site-logo--${props.context}`} src={props.context === 'footer' ? logoGrey: logo} />
+		<img alt="" className={`site-logo site-logo--${props.context}`} src={logo} />
 	);
 
 };

8fac448d7167577f9f98f981619e4338f66285d6
Simon Hudson <simon.hudson@experian.com>
2019-07-25 13:55:06 +0100

    header

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -9,12 +9,7 @@ const Banner = props => {
 	return (
 		<div className="banner__outer">
 			<div className="banner wrap">
-				<div className="banner__primary">
-					<h1 className="banner__title">Hi there, my name's Simon. I am an experienced web designer/front-end developer who specialises in writing lean, standards-compliant HTML, CSS and JavaScript to create accessible and usable websites.</h1>
-				</div>
-				<div className="banner__secondary">
-					<img alt="Simon Hudson" className="banner__img" src={selfImg} />
-				</div>
+				<h1 className="banner__title">Hi there, my name's Simon. I am an experienced web designer/front-end developer who specialises in writing lean, standards-compliant HTML, CSS and JavaScript to create accessible and usable websites.</h1>
 			</div>
 		</div>
 	);

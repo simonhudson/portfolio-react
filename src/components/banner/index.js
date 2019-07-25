68daaa021e27ca6576136a10875e0343d06c121d
Simon Hudson <simon.hudson@experian.com>
2019-07-25 15:00:43 +0100

    banner updates

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import css from './css/styles.scss';
+import ExternalLinks from '~/components/external-links';
 import selfImg from '~/assets/imgs/self.jpg';
 
 const Banner = props => {
@@ -11,6 +12,7 @@ const Banner = props => {
 			<div className="banner wrap">
 				<div className="banner__primary">
 					<h1 className="banner__title">Hi there, my name's Simon. I am an experienced web designer/front-end developer who specialises in writing lean, standards-compliant HTML, CSS and JavaScript to create accessible and usable websites.</h1>
+					<ExternalLinks />
 				</div>
 				<div className="banner__secondary">
 					<img alt="Simon Hudson" className="banner__img" src={selfImg} />
